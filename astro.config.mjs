d502e5a6534b89a3adf3b40b317642ad05357a5c
Tilde Twisttmann <tihe001@stud.kea.dk>
2024-09-05 18:05:54 +0200

    set up

--- astro.config.mjs
+++ astro.config.mjs
@@ -4,5 +4,9 @@ import icon from 'astro-icon';
 
 // https://astro.build/config
 export default defineConfig({
-  integrations: [icon()]
+  integrations: [icon()],
+  scripts: [
+    { src: 'https://fonts.googleapis.com/css2?family=Lato:ital,wght@0,100;0,300;0,400;0,700;0,900;1,100;1,300;1,400;1,700;1,900&family=Cabin:ital,wght@0,400;0,700;1,400;1,700&display=swap', type: 'style' }
+]
 });
+
